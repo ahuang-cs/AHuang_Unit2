2e3883c6fe12f101fd0490a43c137e34790bff38
Andrew Huang <ahuang@sdccd.edu>
2021-03-05 13:34:22 -0800

    Unit 2: Lesson D completed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    public float spawnInterval = 1.5f;
+
     private float xPosRange = 13f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        InvokeRepeating("SpawnRandomAnimal", 3f, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            float randXPos = Random.Range(-xPosRange, xPosRange);
-            int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 randPos = new Vector3(randXPos, 0f, 21f);
+        
+    }
 
-            Instantiate(animalPrefabs[animalPrefabIndex], randPos, animalPrefabs[animalPrefabIndex].transform.rotation);
-        }
+    void SpawnRandomAnimal()
+    {
+        float randXPos = Random.Range(-xPosRange, xPosRange);
+        int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 randPos = new Vector3(randXPos, 0f, 21f);
+
+        Instantiate(animalPrefabs[animalPrefabIndex], randPos, animalPrefabs[animalPrefabIndex].transform.rotation);
     }
 }
